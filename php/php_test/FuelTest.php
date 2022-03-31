8b5fd41a30199ecc563d93d879faa77bde356241
Zachary Robinson <87831656+xnautsing04@users.noreply.github.com>
2022-03-31 16:47:41 -0500

    Update FuelTest.php

--- php/php_test/FuelTest.php
+++ php/php_test/FuelTest.php
@@ -4,61 +4,84 @@ require_once('./php_class/FuelQuote.php');
 
     class FuelTest extends TestCase{
 
+    //Test getting the gallon number.
     public function testFuelGetGallons(): void
     {
         $quoteTester = new FuelQuote(50, "2022-05-07", "cooldude9");
         $this->assertSame($quoteTester -> getGallons(), 50);
     }
+    
+    //Test getting the delivery date.
     public function testFuelGetDate(): void
     {
         $quoteTester = new FuelQuote(50, "2022-05-07", "cooldude9");
         $this->assertSame($quoteTester -> getDate(), "2022-05-07");
     }
+    
+    //Test a gallon number that is not a number.
     public function testFuelNaNGallons(): void
     {
         $quoteTester = new FuelQuote("not a number", "2022-08-30", "cooldude9");
         $this->assertSame($quoteTester -> getGallons(), -1);
     }
+    
+    //Test a gallon number that is negative.
     public function testFuelLowGallons(): void
     {
         $quoteTester = new FuelQuote(-18, "2022-08-30", "cooldude9");
         $this->assertSame($quoteTester -> getGallons(), -1);
     }
+    
+    //Test a delivery date that is not a date.
     public function testFuelNotTime(): void
     {
         $quoteTester = new FuelQuote(230, "on my birthday", "cooldude9");
         $this->assertSame($quoteTester -> getDate(), "1901-01-01");
     }
+    
+    //Test a delivery date that is earlier than the current date.
     public function testFuelEarlyTime(): void
     {
         $quoteTester = new FuelQuote(230, "2022-01-05", "cooldude9");
         $this->assertSame($quoteTester -> getDate(), "1901-01-01");
     }
+    
+    //Test a delivery date that is on the same day.
     public function testFuelSameDay(): void
     {
         $quoteTester = new FuelQuote(230, "now", "cooldude9");
         $this->assertSame($quoteTester -> getDate(), "1901-01-01");
     }
+    
+    //Test a delivery date that is on the next day.
     public function testFuelNextDay(): void
     {
         $quoteTester = new FuelQuote(230, date("Y-m-d", strtotime("now+1 day")), "cooldude9");
         $this->assertSame($quoteTester -> getDate(), "1901-01-01");
     }
+    
+    //Test a delivery date that is two days from now.
     public function testFuelTwoDays(): void
     {
         $quoteTester = new FuelQuote(230, date("Y-m-d", strtotime("now+2 day")), "cooldude9");
         $this->assertSame($quoteTester -> getDate(), date("Y-m-d", strtotime("now+2 day")));
     }
+    
+    //Test an interger price.
     public function testPriceInt(): void
     {
         $quoteTester = new FuelQuote(100, "2022-05-07", "cooldude9");
         $this->assertSame($quoteTester->calculatePrice(), "$250.00");
     }
+    
+    //Test a float price.
     public function testPriceFloat(): void
     {
         $quoteTester = new FuelQuote(101, "2022-05-07", "cooldude9");
         $this->assertSame($quoteTester->calculatePrice(), "$252.50");
     }
+    
+    //Test inserting data into the database.
     public function testInsertData(): void
     {
         $quoteTester = new FuelQuote(1011, "2022-05-07", "cooldude9");
