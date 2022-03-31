19ebcd25ffc6fc2526e5a6e044f26fbb8ab81bd6
Zachary Robinson <87831656+xnautsing04@users.noreply.github.com>
2022-03-31 16:49:42 -0500

    Update insertData.php

--- php/insertData.php
+++ php/insertData.php
@@ -1,5 +1,8 @@
+<!-- General PHP file to insert data -->
+
 <?php
 
+//This ensures that if a user is not logged in, it will direct them to the login page.
 if(isset($_COOKIE["username"])){
     $user = $_COOKIE["username"];
     
@@ -16,5 +19,4 @@ require_once('php_class/FuelQuote.php');
 $userQuote = new FuelQuote($_POST["GallonNumberConf"], $_POST["DelDateConf"], $user);
 
 $userQuote->insertData();
-
 
