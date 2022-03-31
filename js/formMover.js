19796bbea1cee30c014b1b516fd0c9841d48e392
Zachary Robinson <87831656+xnautsing04@users.noreply.github.com>
2022-03-31 16:44:29 -0500

    Update formMover.js

--- js/formMover.js
+++ js/formMover.js
@@ -1,18 +1,24 @@
+//These functions act as links for buttons to work with upon clicking when the buttons are not within forms.
+
+//Takes you to the confirmation page.
 function confirmInfo()
 {
     window.location.href="../pages/fuel_quote_confirmation.html";
 }
 
+//Takes you to the fuel quote denial page.
 function denyInfo()
 {
     window.location.href="../php/fuelQuoteForm.php";
 }
 
+//Takes you to the fuel quote form.
 function formLoader()
 {
     window.location.href="../php/fuelQuoteForm.php";
 }
 
+//Takes you to the fuel quote history page.
 function historyLoader()
 {
     window.location.href = "../php/fuel_quote_history.php";
@@ -23,8 +29,8 @@ function profileLoader()
     window.location.href = "../php/clientProfile.php";
 }
 
+//Takes you to the login screen.
 function loginLoader()
 {
     window.location.href = "../pages/login.html";
 }
-
