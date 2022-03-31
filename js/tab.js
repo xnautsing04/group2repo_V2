95eaa096b74409c158899d3acadd8ec88af7c05f
Zachary Robinson <87831656+xnautsing04@users.noreply.github.com>
2022-03-31 16:44:46 -0500

    Update tab.js

--- js/tab.js
+++ js/tab.js
@@ -1,28 +1,36 @@
+//These functions correspond with the different tabs on top of the screen.
+
+//This takes you to the login page.
 function loginTab()
 {
     window.location.href = "../php/login.php";
 }
 
+//This takes you to the register page.
 function registerTab()
 {
     window.location.href = "../pages/registration.html";
 }
 
+//This takes you to the profile page.
 function profileTab()
 {
     window.location.href = "../php/clientProfile.php";
 }
 
+//This takes you to the fuel quote form.
 function quoteTab()
 {
     window.location.href = "../php/fuelQuoteForm.php";
 }
 
+//This takes you to the fuel quote history page.
 function historyTab()
 {
     window.location.href = "../php/fuel_quote_history.php";
 }
 
+//This logs the user out.
 function logoutTab(){
     window.location.href = "../php/loggingOut.php";
 }
