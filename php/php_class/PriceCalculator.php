5cae415fafff01b9cc6633d1c6e2b2e63060741f
Zachary Robinson <87831656+xnautsing04@users.noreply.github.com>
2022-03-31 16:47:15 -0500

    Update PriceCalculator.php

--- php/php_class/PriceCalculator.php
+++ php/php_class/PriceCalculator.php
@@ -5,8 +5,8 @@ class PriceCalculator{
         //TO BE ADDED LATER
     }
     
+    //This function is temporarily only returning 2.50 as a base rate.
     public static function suggestedPrice(){
         return "$2.50";
     }
 }
-
